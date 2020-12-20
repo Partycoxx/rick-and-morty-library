4e679099e0295a77c6832c3a63d35d3ee0fcb857
Nikita <nikevdokimov34@gmail.com>
2020-12-20 19:02:01 +0300

    Fix routing issues

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Character from "./containers/Character/Character";
 import NotFound from "./containers/NotFound/NotFound";
 import Layout from "./containers/Layout/Layout";
 
-//
-
 import "./App.css";
 
 function App() {
@@ -15,7 +13,7 @@ function App() {
       <Layout>
         <Switch>
           <Route path="/rick-and-morty-library/character/:id" component={Character} />
-          <Route exact path="/rick-and-morty-library" component={Characters} />
+          <Route exact path="/rick-and-morty-library/" component={Characters} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,7 +7,7 @@ export default function Card({ name, id, image }) {
   const { path, url } = useRouteMatch();
 
   return (
-    <Link className="card" to={`${url}/character/${id}`}>
+    <Link className="card" to={`${url}character/${id}`}>
       <div className="card__content-container">
         <h2 className="card__title">{name}</h2>
         <img className="card__image" src={image} alt={name} />
